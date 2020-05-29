439e5261a65bf85b83d2389299f975c40ef97ea1
Everton Oliveira <everton.oliveira@tuta.io>
2020-05-29 01:30:55 -0300

    Teste de function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-alert("meu primeiro js")
+// alert("meu primeiro js")
+/*var lista = ["maça","pera","laranja"];
+lista.push("uva")
+
+//console.log(lista[0]);
+console.log(lista)
+*/
+function clicou (){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar</b>"
+    //alert("Obrigado por clicar")
+}
+
+function redirecionamento (){
+    window.open("https://www.bbc.com/portuguese") //Abre em nova aba
+   // window.location.href = "https://www.bbc.com/portuguese" // Abre na mesma pagina
+}
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+function voltar(elemento){
+elemento.innerHTML = "Passe o mouse aqui"
+}
